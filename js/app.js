695260e0f7e025b2a4374fc92ad0e82e7513f222
Jacob Tham <jacobtham3@gmail.com>
2025-05-19 15:22:37 +0800

    test importing of carddeck after removing bootstrap

--- js/app.js
+++ js/app.js
@@ -1,58 +1,58 @@
-// import { gameCardsDetails } from "./carddeck.js";
-const gameCardsDetails = [
-  { number: 0, suit: "♠", value: 1, display: "A♠", color: "black" },
-  { number: 1, suit: "♠", value: 2, display: "2♠", color: "black" },
-  { number: 2, suit: "♠", value: 3, display: "3♠", color: "black" },
-  { number: 3, suit: "♠", value: 4, display: "4♠", color: "black" },
-  { number: 4, suit: "♠", value: 5, display: "5♠", color: "black" },
-  { number: 5, suit: "♠", value: 6, display: "6♠", color: "black" },
-  { number: 6, suit: "♠", value: 7, display: "7♠", color: "black" },
-  { number: 7, suit: "♠", value: 8, display: "8♠", color: "black" },
-  { number: 8, suit: "♠", value: 9, display: "9♠", color: "black" },
-  { number: 9, suit: "♠", value: 10, display: "10♠", color: "black" },
-  { number: 10, suit: "♠", value: 10, display: "J♠", color: "black" }, // Jack
-  { number: 11, suit: "♠", value: 10, display: "Q♠", color: "black" }, // Queen
-  { number: 12, suit: "♠", value: 10, display: "K♠", color: "black" }, // King
-  { number: 13, suit: "♦", value: 1, display: "A♦", color: "red" },
-  { number: 14, suit: "♦", value: 2, display: "2♦", color: "red" },
-  { number: 15, suit: "♦", value: 3, display: "3♠", color: "red" },
-  { number: 16, suit: "♦", value: 4, display: "4♦", color: "red" },
-  { number: 17, suit: "♦", value: 5, display: "5♦", color: "red" },
-  { number: 18, suit: "♦", value: 6, display: "6♦", color: "red" },
-  { number: 19, suit: "♦", value: 7, display: "7♦", color: "red" },
-  { number: 20, suit: "♦", value: 8, display: "8♦", color: "red" },
-  { number: 21, suit: "♦", value: 9, display: "9♦", color: "red" },
-  { number: 22, suit: "♦", value: 10, display: "10♦", color: "red" },
-  { number: 23, suit: "♦", value: 10, display: "J♦", color: "red" }, // Jack
-  { number: 24, suit: "♦", value: 10, display: "Q♦", color: "red" }, // Queen
-  { number: 25, suit: "♦", value: 10, display: "K♦", color: "red" }, // King
-  { number: 26, suit: "♥", value: 1, display: "A♥", color: "red" },
-  { number: 27, suit: "♥", value: 2, display: "2♥", color: "red" },
-  { number: 28, suit: "♥", value: 3, display: "3♥", color: "red" },
-  { number: 29, suit: "♥", value: 4, display: "4♥", color: "red" },
-  { number: 30, suit: "♥", value: 5, display: "5♥", color: "red" },
-  { number: 31, suit: "♥", value: 6, display: "6♥", color: "red" },
-  { number: 32, suit: "♥", value: 7, display: "7♥", color: "red" },
-  { number: 33, suit: "♥", value: 8, display: "8♥", color: "red" },
-  { number: 34, suit: "♥", value: 9, display: "9♥", color: "red" },
-  { number: 35, suit: "♥", value: 10, display: "10♥", color: "red" },
-  { number: 36, suit: "♥", value: 10, display: "J♥", color: "red" }, // Jack
-  { number: 37, suit: "♥", value: 10, display: "Q♥", color: "red" }, // Queen
-  { number: 38, suit: "♥", value: 10, display: "K♥", color: "red" }, // King
-  { number: 39, suit: "♣", value: 1, display: "A♣", color: "black" },
-  { number: 40, suit: "♣", value: 2, display: "2♣", color: "black" },
-  { number: 41, suit: "♣", value: 3, display: "3♣", color: "black" },
-  { number: 42, suit: "♣", value: 4, display: "4♣", color: "black" },
-  { number: 43, suit: "♣", value: 5, display: "5♣", color: "black" },
-  { number: 44, suit: "♣", value: 6, display: "6♣", color: "black" },
-  { number: 45, suit: "♣", value: 7, display: "7♣", color: "black" },
-  { number: 46, suit: "♣", value: 8, display: "8♣", color: "black" },
-  { number: 47, suit: "♣", value: 9, display: "9♣", color: "black" },
-  { number: 48, suit: "♣", value: 10, display: "10♣", color: "black" },
-  { number: 49, suit: "♣", value: 10, display: "J♣", color: "black" }, // Jack
-  { number: 50, suit: "♣", value: 10, display: "Q♣", color: "black" }, // Queen
-  { number: 51, suit: "♣", value: 10, display: "K♣", color: "black" }, // King
-];
+import { gameCardsDetails } from "./carddeck.js";
+// const gameCardsDetails = [
+//   { number: 0, suit: "♠", value: 1, display: "A♠", color: "black" },
+//   { number: 1, suit: "♠", value: 2, display: "2♠", color: "black" },
+//   { number: 2, suit: "♠", value: 3, display: "3♠", color: "black" },
+//   { number: 3, suit: "♠", value: 4, display: "4♠", color: "black" },
+//   { number: 4, suit: "♠", value: 5, display: "5♠", color: "black" },
+//   { number: 5, suit: "♠", value: 6, display: "6♠", color: "black" },
+//   { number: 6, suit: "♠", value: 7, display: "7♠", color: "black" },
+//   { number: 7, suit: "♠", value: 8, display: "8♠", color: "black" },
+//   { number: 8, suit: "♠", value: 9, display: "9♠", color: "black" },
+//   { number: 9, suit: "♠", value: 10, display: "10♠", color: "black" },
+//   { number: 10, suit: "♠", value: 10, display: "J♠", color: "black" }, // Jack
+//   { number: 11, suit: "♠", value: 10, display: "Q♠", color: "black" }, // Queen
+//   { number: 12, suit: "♠", value: 10, display: "K♠", color: "black" }, // King
+//   { number: 13, suit: "♦", value: 1, display: "A♦", color: "red" },
+//   { number: 14, suit: "♦", value: 2, display: "2♦", color: "red" },
+//   { number: 15, suit: "♦", value: 3, display: "3♠", color: "red" },
+//   { number: 16, suit: "♦", value: 4, display: "4♦", color: "red" },
+//   { number: 17, suit: "♦", value: 5, display: "5♦", color: "red" },
+//   { number: 18, suit: "♦", value: 6, display: "6♦", color: "red" },
+//   { number: 19, suit: "♦", value: 7, display: "7♦", color: "red" },
+//   { number: 20, suit: "♦", value: 8, display: "8♦", color: "red" },
+//   { number: 21, suit: "♦", value: 9, display: "9♦", color: "red" },
+//   { number: 22, suit: "♦", value: 10, display: "10♦", color: "red" },
+//   { number: 23, suit: "♦", value: 10, display: "J♦", color: "red" }, // Jack
+//   { number: 24, suit: "♦", value: 10, display: "Q♦", color: "red" }, // Queen
+//   { number: 25, suit: "♦", value: 10, display: "K♦", color: "red" }, // King
+//   { number: 26, suit: "♥", value: 1, display: "A♥", color: "red" },
+//   { number: 27, suit: "♥", value: 2, display: "2♥", color: "red" },
+//   { number: 28, suit: "♥", value: 3, display: "3♥", color: "red" },
+//   { number: 29, suit: "♥", value: 4, display: "4♥", color: "red" },
+//   { number: 30, suit: "♥", value: 5, display: "5♥", color: "red" },
+//   { number: 31, suit: "♥", value: 6, display: "6♥", color: "red" },
+//   { number: 32, suit: "♥", value: 7, display: "7♥", color: "red" },
+//   { number: 33, suit: "♥", value: 8, display: "8♥", color: "red" },
+//   { number: 34, suit: "♥", value: 9, display: "9♥", color: "red" },
+//   { number: 35, suit: "♥", value: 10, display: "10♥", color: "red" },
+//   { number: 36, suit: "♥", value: 10, display: "J♥", color: "red" }, // Jack
+//   { number: 37, suit: "♥", value: 10, display: "Q♥", color: "red" }, // Queen
+//   { number: 38, suit: "♥", value: 10, display: "K♥", color: "red" }, // King
+//   { number: 39, suit: "♣", value: 1, display: "A♣", color: "black" },
+//   { number: 40, suit: "♣", value: 2, display: "2♣", color: "black" },
+//   { number: 41, suit: "♣", value: 3, display: "3♣", color: "black" },
+//   { number: 42, suit: "♣", value: 4, display: "4♣", color: "black" },
+//   { number: 43, suit: "♣", value: 5, display: "5♣", color: "black" },
+//   { number: 44, suit: "♣", value: 6, display: "6♣", color: "black" },
+//   { number: 45, suit: "♣", value: 7, display: "7♣", color: "black" },
+//   { number: 46, suit: "♣", value: 8, display: "8♣", color: "black" },
+//   { number: 47, suit: "♣", value: 9, display: "9♣", color: "black" },
+//   { number: 48, suit: "♣", value: 10, display: "10♣", color: "black" },
+//   { number: 49, suit: "♣", value: 10, display: "J♣", color: "black" }, // Jack
+//   { number: 50, suit: "♣", value: 10, display: "Q♣", color: "black" }, // Queen
+//   { number: 51, suit: "♣", value: 10, display: "K♣", color: "black" }, // King
+// ];
 
 //---------------------------- Constants ----------------------------//
 
